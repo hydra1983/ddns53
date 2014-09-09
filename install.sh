24b85b199c6e7a2d620ae07abd085c6184d4dc01
Edison Guo <hydra1983@gmail.com>
2014-09-09 22:54:16 +0100

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 apt-get update
 apt-get install python-pip sed curl -y
 pip install boto --upgrade
-wget -O https://github.com/hydra1983/ddns53/raw/master/etc/init.d/ddns53.sh > /etc/init.d/ddns53
-mkdir /etc/ddns53
-wget -O https://github.com/hydra1983/ddns53/raw/master/etc/ddns53/ddns53.conf > /etc/ddns53/ddns53.conf
-vi /etc/ddns53/ddns53.conf
+
+curl -sSL https://github.com/hydra1983/ddns53/raw/master/etc/init.d/ddns53.sh > /etc/init.d/ddns53
 chmod +x /etc/init.d/ddns53
 update-rc.d ddns53 defaults
+
+[ ! -d /etc/ddns53 ] && mkdir /etc/ddns53
+curl -sSL https://github.com/hydra1983/ddns53/raw/master/etc/ddns53/ddns53.conf > /etc/ddns53/ddns53.conf
+vi /etc/ddns53/ddns53.conf
+
 /etc/init.d/ddns53 start
